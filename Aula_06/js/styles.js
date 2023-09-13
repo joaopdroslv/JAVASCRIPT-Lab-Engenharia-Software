e77986693d91eeae3d5e4e292d94b9cfe46d402d
JOÃO PEDRO <142755923+joaopdroslv@users.noreply.github.com>
2023-09-12 22:02:37 -0300

    Add files via upload

---
+++ Aula_06/js/styles.js
@@ -0,0 +1,70 @@
+const bodyStyle = {
+  display: "flex",
+  "justify-content": "center",
+  "align-items": "center",
+};
+
+const calculadoraBody = {
+  width: "360px",
+  height: "600px",
+  border: "1px solid black",
+  "border-radius": "10px",
+  display: "flex",
+  "flex-direction": "column",
+  padding: "20px",
+
+  // testing
+  "background-color": "black",
+};
+
+const result = {
+  width: "100%",
+  height: "100px",
+  color: "white",
+  "font-size": "50px",
+
+  display: "flex",
+  "justify-content": "flex-end",
+  "align-items": "center",
+
+  "margin-top": "10px",
+  "margin-bottom": "10px",
+};
+
+const buttonsContainer = {
+  padding: "10px",
+  width: "100%",
+  display: "flex",
+  "flex-wrap": "wrap",
+};
+
+const button = {
+  width: "80px",
+  height: "80px",
+  border: "none",
+  "border-radius": "50px",
+  "font-size": "30px",
+  "margin-top": "5px",
+  "margin-left": "5px",
+  cursor: "pointer",
+};
+
+const button_zero = {
+  width: "160px",
+  height: "80px",
+  border: "none",
+  "border-radius": "50px",
+  "font-size": "30px",
+  "text-align": "left",
+  "padding-left": "30px",
+  cursor: "pointer",
+};
+
+export {
+  bodyStyle,
+  calculadoraBody,
+  result,
+  buttonsContainer,
+  button,
+  button_zero,
+};
